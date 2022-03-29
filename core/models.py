b348c5bea1f85f6591dd6939919e111a688be72a
Kush2022 <felixkuria12@gmail.com>
2022-03-29 11:14:14 +0300

    first commit

---
+++ core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    create = models.DateField(auto_now_add=True)
+    update = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title.upper()
+
+    class Meta:
+        ordering = ['create']
